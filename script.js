df1804987e172e198f7e020f6842d705050542be
saurabh <saurabhshinde779845@gmail.com>
2024-08-21 17:28:20 +0530

    updated on 21 aug

--- script.js
+++ script.js
@@ -1,20 +1,103 @@
 
+// let playerinfo=[
 
-function login()
-{
-    let email=document.getElementById("email").value ;
-    let pass=document.getElementById("pass").value ;
+//      {
+//         name: "virat",
+//         age: 32,
+//         sport:"cricket",
+//         score:1000
+//     },
+    
+//  {
+//         name:"hardik",
+//         age: 35,
+//         sport:"cricket",
+//         score:1000
+//     },
+    
+//     {
+//         name: "rohit",
+//         age: 45,
+//         sport:"cricket",
+//         score:1000
+//     },
+    
+//     {
+//         name: "shami",
+//         age: 40,
+//         sport:"cricket",
+//         score:1000
+//     },
+    
+//    {
+//         name:"dhoni",
+//         age: 46,
+//         sport:"cricket",
+//         score:1000
+//     }
+// ];
+// console.log(playerinfo);
 
-    if( !pass || !email){
-        alert("Please fill in all fields");
-    }
-    else if(email=="saurabh123@gamil.com" && pass=="779845"){
-        alert("Login Successfull");
-        window.open("test.html");
-    }
-    else{
-        alert("Invalid Email or Password");
-    }
+// var username="saurabh_"
+
+// function validate(){
+//     let userinput= document.getElementById("userinput").value;
+
+
+// if (userinput==username){
+//     console.log("username is valid ");
+//     window.href="home.html";
+// }
+// }
+
+
+
+
+// function validate(){
+
+
+//     let userinput=document.getElementById("userinput").value;
+//     let pasword=document.getElementById("pasword").value;
+
+//     if(!userinput || !pasword){
+//         alert("Please enter both username and password");
+//     }
+
+//     else if (userinput=="saurabh_" && pasword=="saurabh@123")
+//         {
+//         console.log("username and password is valid ");
+//         window.open("home.html");
+//     }
+//     else{
+//         console.log("username and password is invalid");
+//         alert("username and password is invalid")
+//     }
+
+// }
+
+
+// addition function
+
+// function add(){
+    
+//     let num1 = parseInt(document.getElementById("num1").value);
+//     let num2 = parseInt(document.getElementById("num2").value);
+
+//     let sum = num1+num2;
+
+//     alert(sum);
+// }
+
+
+// var a="saurabh";
+
+//  console.log(typeof(a));
+
+function add(){
+    let Box=document.getElementById("Box")
+    let userinput=(document.getElementById("num2").value);
+
+    Box.style.backgroundColor=userinput;
+    Box.style.borderRadius=userinput;
+    Box.style.transform=userinput;
 }
-
-
